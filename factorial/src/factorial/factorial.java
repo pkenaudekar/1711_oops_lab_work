e4dd52f92f989465b14eee29ebba2e1b872273cd
Pratik Kenaudekar <Pratik Kenaudekar@SCORP>
2018-07-17 15:01:21 +0530

    3rd Commit

--- factorial/src/factorial/factorial.java
+++ factorial/src/factorial/factorial.java
@@ -1,16 +1,12 @@
 package factorial;
-import java.util.Scanner;
 import factorial.fact;
 
 public class factorial {
 	
-	private static Scanner in;
-
 		public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Enter the number to find its factorial:");
-		in = new Scanner(System.in);		 
-		int num = in.nextInt();
+		int num = 5;
+		System.out.println("The number to find its factorial is:" + num);
 		System.out.println("Factorial of "+ num + " is " + fact.fact_cal(num));
 
 	}
